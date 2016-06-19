6d8d46e3774cf7147bd84624ec6779d671768af2
Paulus Esterhazy <pesterhazy@gmail.com>
2016-06-19 21:46:31 +0200

    go

--- presentation/index.js
+++ presentation/index.js
@@ -99,15 +99,42 @@ export default class Presentation extends React.Component {
           </Slide>
 
             <Slide transition={["fade"]} bgColor="#222222">
-            <Image src={images.react.replace("/", "")} margin="0px auto 40px" height="100px"/>
-            <Text textColor="white" textSize="1.5em" margin="20px 0px 0px" bold>
-            React Native
-        </Text>
-              <CodePane
+            <Image src={images.react.replace("/", "")} margin="0px auto 20px" height="100px"/>
+            <List textColor="white">
+            <ListItem>single entry point to JavaScript API: registerComponent</ListItem>
+            <ListItem>React + react-dom vs React + react-native</ListItem>
+            </List>
+            <CodePane
               lang="javascript"
               source={require("raw!../assets/example.js")}
               margin="20px auto"
             />
+            </Slide>
+
+            <Slide transition={["fade"]} bgColor="#222222">
+            <Image src={images.react.replace("/", "")} margin="0px auto 20px" height="100px"/>
+            <Text textColor="white" textSize="1.5em" margin="20px 0px 0px" bold>
+            Value proposition 1/2
+        </Text>
+            <List textColor="white">
+            <Appear><ListItem>The magic of react</ListItem></Appear>
+            <Appear><ListItem>Browser-like development experience</ListItem></Appear>
+            <Appear><ListItem>Use JavaScript or any compile-to-JavaScript language (TypeScript, ClojureScript, Elm)</ListItem></Appear>
+            </List>
+            </Slide>
+
+            <Slide transition={["fade"]} bgColor="#222222">
+            <Image src={images.react.replace("/", "")} margin="0px auto 20px" height="100px"/>
+            <Text textColor="white" textSize="1.5em" margin="20px 0px 0px" bold>
+            Value proposition 2/2
+        </Text>
+            <List textColor="white">
+            <Appear><ListItem>Familiar runtime: runs on top of JavasScriptCore (on iOS) or v8 (on Android)</ListItem></Appear>
+            <Appear><ListItem>Familiar event loop</ListItem></Appear>
+            <Appear><ListItem>Familiar React ecosystem (redux, reagent)</ListItem></Appear>
+            <Appear><ListItem>Shared code between Android and iOS</ListItem></Appear>
+            <Appear><ListItem>Potential non-UI code reuse between web and app</ListItem></Appear>
+            </List>
             </Slide>
 
 
